f516a160ef8a3062c28bbb4ac4181ce6f6184d18
bbaoqaq <baoziqaq@qq.com>
2023-07-02 10:02:15 +0800

    自动创建日志路径

--- wowsCheaterViewer/Logger.cs
+++ wowsCheaterViewer/Logger.cs
@@ -12,15 +12,21 @@ namespace wowsCheaterViewer
     class Logger
     {
         //设定日志路径
-        private static string logFolder = "log";
-        private static string defaultLogFile = Path.Combine(logFolder, DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss.fff") + ".log");
+        private static readonly string logFolder = "log";
+        private static readonly string defaultLogFile = Path.Combine(logFolder, DateTime.Now.ToString("yyyy-MM-dd HH.mm.ss.fff") + ".log");
+        static Logger()
+        {
+            if (!Directory.Exists(logFolder))
+                Directory.CreateDirectory(logFolder);
+        }
+
         //用锁定防止并行占进程
         private static readonly object writerLock = new object();
         public static void logWrite(string message)
         {
             lock (writerLock)
             {
-                File.AppendAllText(defaultLogFile,$"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message} {Environment.NewLine}");
+                File.AppendAllText(defaultLogFile, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message} {Environment.NewLine}");
             }
         }
     }
